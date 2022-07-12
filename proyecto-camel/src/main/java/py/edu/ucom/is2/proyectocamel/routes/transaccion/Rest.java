6fd40b586f618732ee1929790da2c1c3f8409a32
fabioenrico16 <75529324+fabioenrico16@users.noreply.github.com>
2022-07-12 15:44:18 -0400

    Add files via upload

---
+++ proyecto-camel/src/main/java/py/edu/ucom/is2/proyectocamel/routes/transaccion/Rest.java
@@ -0,0 +1,46 @@
+package py.edu.ucom.is2.proyectocamel.routes.transaccion;
+
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Rest extends RouteBuilder {
+
+
+
+
+	@Override
+	public void configure() throws Exception {
+		
+		restConfiguration().component("servlet").bindingMode(RestBindingMode.off);
+		rest().path("/api")
+		.consumes("application/json")
+		.produces("application/json")	
+		
+			
+			.post("/transferencia")
+			.to("direct:procesar-transferencia");		
+		
+		
+		from("direct:procesar-transferencia")
+		.setExchangePattern(ExchangePattern.InOnly) 
+			.choice()
+			.when(header("BANCO_DESTINO").contains("ATLAS")).to("activemq:Acosta-ATLAS-IN").endChoice()
+			.when(header("BANCO_DESTINO").contains("ITAU")).to("activemq:Acosta-ITAU-IN").endChoice()
+			.otherwise()
+				.transform().constant("El valor enviado no ex valido")
+		.setExchangePattern(ExchangePattern.InOut) ;
+		
+		
+		
+		
+	}
+
+}
+
+//http://localhost:8080/camel/api/enviar
+
+
+
